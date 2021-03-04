2360beef9926fe4ff85882be84287476d2958d5b
usuario <usuario@DJdaniel10>
2021-03-04 10:33:28 -0500

    Creación de todas las posibles ventanas de la GUI, Finalización de la primera ventana.

--- src/Registrarse.java
+++ src/Registrarse.java
@@ -8,12 +8,12 @@
  *
  * @author usuario
  */
-public class Tienda extends javax.swing.JFrame {
+public class Registrarse extends javax.swing.JFrame {
 
     /**
-     * Creates new form Tienda
+     * Creates new form Registrarse
      */
-    public Tienda() {
+    public Registrarse() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Tienda extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Tienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Registrarse.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Tienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Registrarse.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Tienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Registrarse.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Tienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Registrarse.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Tienda().setVisible(true);
+                new Registrarse().setVisible(true);
             }
         });
     }
